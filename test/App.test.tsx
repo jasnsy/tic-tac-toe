e2dbed3e1c6ab2286574757b22f2cfd4dfca9081
Jason <jason@Jasons-MacBook-Pro.local>
2022-08-23 12:25:29 -0600

    update test to use createRoot

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { act } from 'react-dom/test-utils';
-import * as ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "../src/App";
 
-/**
- * @jest-environment jsdom
- */
-
 describe('App', function () {
-   it('should display pass in number', function () {
-       let container = document.createElement('div');
-       document.body.appendChild(container);
-       act(() => {
-           ReactDOM.render(<App />, container);
-       })
+    it('should display pass in number', function () {
+        let container = document.createElement('div');
+        document.body.appendChild(container);
+        act(() => {
+            const root = createRoot(container);
+            root.render(<App />);
+        })
        const header = container.querySelector('h1');
        expect(header.textContent).toBe("Create React App from Scratch Using Typescript!")
-   });
+    });
 });
